be55ffbacfa9f9bc04d70e2db2fe71ca12a43323
Danilo V Roque <daniloviniciusroque@gmail.com>
2019-06-25 19:38:49 -0300

    Comments Action

--- frontend/src/actions/comments.js
+++ frontend/src/actions/comments.js
@@ -1,8 +1,120 @@
-export const RECEIVE_COMMENTS = 'RECEIVE_COMMENTS'
+import {
+    getComment,
+    getComments,
+    createComment,
+    updateComment,
+    deleteComment,
+    voteComment
+} from '../api/api'
+import { generateId } from '../api/helpers'
 
-export function receiveComments (comments) {
+export const RECEIVE_COMMENTS = 'RECEIVE_COMMENTS'
+export const RECEIVE_COMMENT = 'RECEIVE_COMMENT'
+
+function getCommentsAction (comments) {
     return {
         type: RECEIVE_COMMENTS,
         comments
+    }
+}
+
+function getCommentAction (comment) {
+    return {
+        type: RECEIVE_COMMENT,
+        comment
+    }
+}
+
+export function handleGetComments (postId) {
+    return (dispatch) => {
+        getComments(postId)
+            .then((c) => {
+                dispatch(getCommentsAction(c))
+            })
+    }
+}
+
+export function handleGetComment (commentId) {
+    return (dispatch) => {
+        getComment(commentId)
+            .then((c) => {
+                dispatch(getCommentAction(c))
+            })
+    }
+}
+
+export const ADD_COMMENT = 'ADD_COMMENT'
+
+function addCommentAction (comment) {
+    return {
+        type: ADD_COMMENT,
+        comment
+    }
+}
+
+export function handleAddComment (comment) {
+    const {
+        body, author, parentId
+    } = comment
+    return (dispatch) => {
+        createComment({
+            id: generateId(),
+            timestamp: new Date().getTime(),
+            body,
+            author,
+            parentId
+        })
+        .then((c) => {
+            dispatch(addCommentAction(c))
+        })
+    }
+}
+
+export const UPDATE_COMMENT = 'UPDATE_COMMENT'
+
+function updateCommentAction (comment) {
+    return {
+        type: UPDATE_COMMENT,
+        comment
+    }
+}
+
+export function handleUpdateComment (comment) {
+    const { body } = comment
+    return (dispatch) => {
+        updateComment({
+            timestamp: new Date().getTime(),
+            body
+        })
+        .then((c) => {
+            dispatch(updateCommentAction(c))
+        })
+    }
+}
+
+export const DELETE_COMMENT = 'DELETE_COMMENT'
+
+function deleteCommentAction (commentId) {
+    return {
+        type: DELETE_COMMENT,
+        commentId
+    }
+}
+
+export function handleDeleteComment (commentId) {
+    return (dispatch) => {
+        deleteComment(commentId)
+            .then((c) => {
+                dispatch(c)
+            })
+    }
+}
+
+export const VOTE = { UP: 'upVote', DOWN: 'downVote' }
+
+export function handleVoteComment (commentId, option) {
+    return (dispatch) => {
+        voteComment(commentId, option)
+            .then((c) => updateCommentAction(c))
     }
 }

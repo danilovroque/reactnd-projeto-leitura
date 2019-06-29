76db36e3c1d109edfc772dfd5245d80351e17cf1
Danilo V Roque <daniloviniciusroque@gmail.com>
2019-06-29 12:59:53 -0300

    Fixing error to votePost

--- frontend/src/components/Posts/Post.js
+++ frontend/src/components/Posts/Post.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { Grid } from 'semantic-ui-react'
 import { handleVotePost } from '../../actions/posts'
 import PostVote from './PostVote'
@@ -24,4 +25,4 @@ class Post extends Component {
     }
 }
 
-export default Post
+export default connect()(Post)
